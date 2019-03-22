9b5dee51c79b5359a7c927f319613e1a4b8e6959
Irving Aparicio <irving.aparicio.pro@gmail.com>
2019-03-22 16:38:28 -0600

    Upload functionality JS

--- javascript/main-js.js
+++ javascript/main-js.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    $("#message").hide();
+
     $('#inversion').keyup(function (event) {
 
         if (event.which >= 37 && event.which <= 40) {
@@ -10,17 +12,26 @@ $(document).ready(function () {
             return value
                 .replace(/\D/g, "")
                 .replace(/([0-9])([0-9]{2})$/, '$1.$2')
-                //.replace(/\B(?=(\d{3})+(?!\d)\.?)/g, ",");
+                .replace(/\B(?=(\d{3})+(?!\d)\.?)/g, ",");
         });
+    });
 
-        $("#calculate").click(function () {
-            var inversion = $("#inversion").val();
-            var time = $("#time").val();
-            var interes = 0.05;
+    $("#interes").focusin(function () {
+        $("#message").show();
+    });
 
-            var ganancia = inversion * (interes * (time / 365));
-            console.log(inversion);
-            $("#results").text(ganancia.toString() + "$");
-        });
+    $("#interes").focusout(function () {
+        $("#message").hide();
+    });
+
+    $("#calculate").click(function () {
+        var time = $("#time").val();
+        var inversion = $("#inversion").val().replace(/,/g, ''),
+            asANumber = +inversion;
+        var interes = $("#interes").val();
+
+        var ganancia = inversion * ((interes / 100) * (time / 365));
+
+        $("#results").text("$" + parseFloat(ganancia.toString().match(/^\d+(?:\.\d{0,2})?/)));
     });
 });
